3be49574dfbd23169f12383ecca510d333ff4a01
Javier <javierrumo2@gmail.com>
2026-04-03 10:14:50 +0200

    local

--- app/(app)/events/page.tsx
+++ app/(app)/events/page.tsx
@@ -7,9 +7,6 @@ export default function DestacadosPage() {
   return (
     <div className="m-0">
       {/* Encabezado de la página */}
-      <div className='w-full bg-gradient-to-b from-gray-900 to-transparent absolute top-0 left-0 z-991'>
-        <Header />
-      </div>
       <Eventos />
       <BottomNav />
     </div>

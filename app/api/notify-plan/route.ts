43637419837fe23fda6330af7ba057b0e1c8634e
Javier <javierrumo2@gmail.com>
2026-04-10 22:16:38 +0200

    email

--- app/api/notify-plan/route.ts
+++ app/api/notify-plan/route.ts
@@ -2,7 +2,7 @@ import { getUser } from "@/lib/auth/get-user";
 import { NextResponse } from "next/server";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 
 export async function POST(req: Request) {
   const user = await getUser();
